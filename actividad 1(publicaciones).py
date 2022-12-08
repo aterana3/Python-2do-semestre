d8b8d56d10ddc3c56c876626480311f915e8ba16
Anthony Terán <108620290+aterana3@users.noreply.github.com>
2022-12-07 21:32:44 -0500

    Mejoras

--- actividad 1(publicaciones).py
+++ actividad 1(publicaciones).py
@@ -5,26 +5,59 @@ import json
 
 archivo = open("publicacion.json")
 contenido = json.load(archivo)
+profesores = []
 
-
-def filtrar():
-    profesores = []
-    for personal in contenido:
-        cedula = personal['cedula']
-        apellidos = personal['apellidos']
-        datos = {'cedula': cedula,
-                 'apellidos': apellidos,
-                 'articulos': 0
-                 }
+def construir():
+    for docente in contenido:
+        cedula = docente['cedula']
+        apellidos = docente['apellidos']
+        datos = {
+            'cedula': cedula,
+            'apellidos': apellidos,
+            'publicaciones': 0,
+            'tipos': [],
+            'areas': []
+        }
         if datos not in profesores:
-             profesores.append(datos)
-    for docentes in profesores:
-        cedula = docentes['cedula']
+            profesores.append(datos)
+    publicaciones()
+    areas()
+    imprimir()
+
+
+def publicaciones():
+    for docente in profesores:
+        cedula = docente['cedula']
         for profesor in contenido:
             if profesor['cedula'] == cedula:
-                docentes['articulos'] += 1
-    print(profesores)
+                docente['publicaciones'] += 1
+
+
+def areas():
+    for docentes in profesores:
+          cedula = docentes['cedula']
+          areas_trabajadas = docentes['areas']
+          for profesor in contenido:
+            if profesor['cedula'] == cedula:
+                area = profesor['area']
+                if not area in areas_trabajadas:
+                   areas_trabajadas.append(area);
+
+
+def imprimir():
+    for docente in profesores:
+        print(" ")
+        print("==========================")
+        print(f"Nombre: {docente['apellidos']}")
+        print(f"Cedula: {docente['cedula']}")
+        print(f"Publicaciones: {docente['publicaciones']}")
+        areas = docente['areas'];
+        print('Areas trabajadas:')
+        for key in areas:
+            print(key)
+        print("==========================")
+        print(" ")
 
 
 if __name__ == "__main__":
-    filtrar();
+    construir();
