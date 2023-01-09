0d5806d309cac2d540758c223c6f233172e2d6c5
Anthony Terán <108620290+aterana3@users.noreply.github.com>
2023-01-09 12:17:46 -0500

    Add files via upload

---
+++ Taller 4/hoja_vida.py
@@ -0,0 +1,23 @@
+import pdfkit
+import jinja2
+from datetime import datetime
+import wkhtmltopdf
+
+data = {}
+data['nombre'] = 'Anthony Emiliano'
+data['hora'] = '60'
+data['fecha'] = datetime.now().strftime("%m %b %Y")
+
+
+content = {'data': data}
+
+template_loader = jinja2.FileSystemLoader('D:/Anthony/tecnicas_programcionA1/Taller 4')
+template_env = jinja2.Environment(loader=template_loader)
+
+template_html = 'index.html'
+template = template_env.get_template(template_html)
+output_text = template.render(content)
+path_wkhtmltopdf = b"C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe"
+config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
+output_pdf = 'certificado.pdf'
+pdfkit.from_string(output_text, output_pdf, configuration=config)
